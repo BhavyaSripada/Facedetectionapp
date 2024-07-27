330c418637d922316e6283cd66b4268cefe703b9
BhavyaSripada <bhavya.sripada@gmail.com>
2024-07-27 09:59:06 +0530

    Removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React from 'react';
 import Home from './Home';
 import RealTime from './RealTime';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
